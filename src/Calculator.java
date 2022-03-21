c1bc4a777d71fd3c0086470d11333a679e9a0c5c
Aidan Copell <aidancopell@aidans-air-2.quinnipiac.edu>
2022-03-21 16:58:44 -0400

    Took two numbers from user and added them together

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,12 @@
-
+import java.util.Scanner;
 public class Calculator {
-	public static void main (String [] args) {
-		
+	public static void main (String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter in two whole numbers.");
+		int wholeNumberOne = scanner.nextInt();
+		int wholeNumberTwo = scanner.nextInt();
+		System.out.print(wholeNumberOne  + wholeNumberTwo);
 	}
+	
 }
