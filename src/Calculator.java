9396d03ffce5be6d7a710df7a089954d67dc0d46
Aidan Copell <aidancopell@aidans-air-2.quinnipiac.edu>
2022-03-28 16:42:34 -0400

    added a subtract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,16 @@ public class Calculator {
 		System.out.println("Enter in two whole numbers.");
 		int wholeNumberOne = scanner.nextInt();
 		int wholeNumberTwo = scanner.nextInt();
-		System.out.print(wholeNumberOne  + wholeNumberTwo);
+		System.out.println(wholeNumberOne  + wholeNumberTwo);
+		Scanner scanner2 = new Scanner(System.in);
+		System.out.println("Would you like to add or subtract the two numbers?");
+		String operationOne = scanner.nextLine();
+		if (operationOne == "add") {
+			System.out.print(wholeNumberOne + wholeNumberTwo);
+		} else if (operationOne == "subtract") {
+			System.out.print(wholeNumberOne - wholeNumberTwo);
+		}
+			
 	}
 	
 }
